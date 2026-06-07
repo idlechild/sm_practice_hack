56cf106d8197cf606b0d68ff1e38e98147ce8c74
idle <idlechild123@gmail.com>
2026-06-06 20:15:19 -0500

    Protect practice hack WRAM against x-ray in some cases

--- src/preserve_wram.asm
+++ src/preserve_wram.asm
@@ -120,3 +120,229 @@ spacetime_routine:
 
 %endfree(90)
 
+
+if !FEATURE_PAL
+org $91BE10
+else
+org $91BEB8
+endif
+    dw xray_offscreen_aim_right
+
+if !FEATURE_PAL
+org $91BE18
+else
+org $91BEC0
+endif
+    dw xray_offscreen_horizontal
+
+if !FEATURE_PAL
+org $91C555
+else
+org $91C5FD
+endif
+    dw xray_onscreen_horizontal
+
+
+%startfree(91)
+
+xray_offscreen_aim_right:
+{
+    PHP : %ai16()
+    LDA $18 : DEC : ASL : TAY
+    CPY #$5CF8 : BCS .preserve
+  .vanilla
+if !FEATURE_PAL
+    JMP $BE22
+else
+    JMP $BECA
+endif
+
+  .preserve
+    CPY #$8008 : BCS .vanilla
+    LDA $16 : STA $22 : STA $24
+
+  .loop_left_offscreen
+    LDA $22 : CLC : ADC $1E : STA $22
+    BCS .left_found_screen
+    DEY #2 : BPL .loop_left_offscreen
+    STZ $12
+    BRA .left_edge_end
+
+  .left_found_screen
+    CPY #$5CF8 : BCC .store_left_offscreen
+    CPY #$6708 : BCC .prepare_left_onscreen
+  .store_left_offscreen
+    LDA $23 : ORA #$FF00 : STA [$00],Y
+  .prepare_left_onscreen
+    INY #2 : STY $12
+    TYA : SEC : SBC #$0004 : TAY
+
+  .loop_left_onscreen
+    LDA $22 : CLC : ADC $1E : STA $22
+    BCS .left_lost_screen
+    CPY #$5CF8 : BCC .loop_left_store
+    CPY #$6708 : BCC .loop_left_dey
+  .loop_left_store
+    LDA $23 : ORA #$FF00 : STA [$00],Y
+  .loop_left_dey
+    DEY #2 : BPL .loop_left_onscreen
+    BRA .left_edge_end
+
+  .left_lost_screen
+    LDA #$00FF
+  .left_first_lost_loop
+    CPY #$6708 : BCC .left_skip_loop
+    STA [$00],Y
+    DEY #2 : BRA .left_first_lost_loop
+  .left_skip_loop
+    CPY #$5CF8 : BCC .left_second_lost_loop
+    DEY #2 : BRA .left_skip_loop
+  .left_second_lost_loop
+    STA [$00],Y
+    DEY #2 : BPL .left_second_lost_loop
+
+  .left_edge_end
+    LDA $18 : ASL : TAY
+
+  .loop_right_offscreen
+    LDA $24 : CLC : ADC $20 : STA $24
+    BCS .right_found_screen
+    INY #2 : CPY #$01CC : BMI .loop_right_offscreen
+    STY $14
+    BRA .right_edge_end
+
+  .right_found_screen
+    CPY #$5CF8 : BCC .store_right_offscreen
+    CPY #$6708 : BCC .prepare_right_onscreen
+  .store_right_offscreen
+    LDA $25 : ORA #$FF00 : STA [$00],Y
+  .prepare_right_onscreen
+    DEY #2 : STY $14
+    TYA : CLC : ADC #$0004 : TAY
+
+  .loop_right_onscreen
+    LDA $24 : CLC : ADC $20 : STA $24
+    BCS .right_lost_screen
+    CPY #$5CF8 : BCC .loop_right_store
+    CPY #$6708 : BCC .loop_right_iny
+  .loop_right_store
+    LDA $25 : ORA #$FF00 : STA [$00],Y
+  .loop_right_iny
+    INY #2 : CPY #$01CC : BMI .loop_right_onscreen
+    BRA .right_edge_end
+
+  .right_lost_screen
+    LDA #$00FF
+    CPY #$5CF8 : BCC .right_lost_loop
+    CPY #$6708 : BCC .right_lost_skip
+  .right_lost_loop
+    STA [$00],Y
+  .right_lost_skip
+    INY #2 : CPY #$01CC : BMI .right_lost_loop
+
+  .right_edge_end
+    LDY $12
+    LDA #$00FF
+  .final_loop
+    CPY #$5CF8 : BCC .final_loop_store
+    CPY #$6708 : BCC .final_loop_skip
+  .final_loop_store
+    STA [$00],Y
+  .final_loop_skip
+    INY #2 : CPY $14
+    BMI .final_loop : BEQ .final_loop
+    PLP
+    RTS
+}
+
+xray_offscreen_horizontal:
+{
+    PHP : %ai16()
+    LDA $18 : DEC : ASL : TAY
+    CPY #$5CF8 : BCS .preserve
+  .vanilla
+if !FEATURE_PAL
+    JMP $C473
+else
+    JMP $C51B
+endif
+
+  .preserve
+    CPY #$8008 : BCS .vanilla
+    CPY #$6708 : BCC .skip_top_loop
+    LDA #$FF00 : STA [$00],Y
+    PHY
+    INY #2
+  .bottom_loop
+    LDA [$00],Y : CMP #$00FF : BEQ .bottom_loop_end
+    LDA #$00FF : STA [$00],Y
+    INY #2 : CPY #$01CC : BMI .bottom_loop
+  .bottom_loop_end
+    PLY
+    DEY #2
+  .first_top_loop
+    CPY #$6708 : BCC .skip_top_loop
+    LDA [$00],Y : CMP #$00FF : BEQ .end_top_loop
+    LDA #$00FF : STA [$00],Y
+    DEY #2 : BPL .first_top_loop
+    BRA .end_top_loop
+  .skip_top_loop
+    CPY #$5CF8 : BCC .second_top_loop
+    DEY #2 : BRA .skip_top_loop
+  .second_top_loop
+    LDA [$00],Y : CMP #$00FF : BEQ .end_top_loop
+    LDA #$00FF : STA [$00],Y
+    DEY #2 : BPL .second_top_loop
+  .end_top_loop
+    PLP
+    RTS
+}
+
+xray_onscreen_horizontal:
+{
+    PHP : %ai16()
+    LDA $18 : DEC : ASL : TAY
+    CPY #$5CF8 : BCS .preserve
+  .vanilla
+if !FEATURE_PAL
+    JMP $C8F8
+else
+    JMP $C9A0
+endif
+
+  .preserve
+    CPY #$8008 : BCS .vanilla
+    CPY #$6708 : BCC .skip_top_loop
+    LDA $12 : CMP #$0040 : BNE .not40
+    LDA $17 : ORA #$FF00
+    BRA .store_first
+  .not40
+    LDA $16 : AND #$FF00
+  .store_first
+    STA [$00],Y
+    PHY
+    INY #2
+    LDA #$00FF
+  .bottom_loop
+    STA [$00],Y
+    INY #2 : CPY #$01CC : BMI .bottom_loop
+    PLY
+    DEY #2
+  .first_top_loop
+    CPY #$6708 : BCC .skip_top_loop
+    STA [$00],Y
+    DEY #2 : BPL .first_top_loop
+    BRA .end_top_loop
+  .skip_top_loop
+    CPY #$5CF8 : BCC .second_top_loop
+    DEY #2 : BRA .skip_top_loop
+  .second_top_loop
+    STA [$00],Y
+    DEY #2 : BPL .second_top_loop
+  .end_top_loop
+    PLP
+    RTS
+}
+
+%endfree(91)
+
